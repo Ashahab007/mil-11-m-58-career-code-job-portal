8c728fb6bb0c2b6cdd064901c098df0a02e0a18d
Ashahab007 <ashahab007@gmail.com>
2025-05-25 23:09:14 +0600

    created Navbar, Footer, router and layout setup

---
+++ src/router/Router.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../pages/layout/RootLayout";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
